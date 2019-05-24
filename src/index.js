e2581aae1c1db7475bba6e9e6882f6b327be0d62
bishuu1 <tlz98x@gmail.com>
2019-05-24 05:02:28 -0400

    again

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+//Inicializacion
+const app = express();
+
+//Configuracion
+app.set('port', process.env.PORT || 3000);
+app.set('viws', path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Variables Globales
+app.use((req, res, next) => {
+
+    next();
+});
+
+//Route
+app.use(require('./routes'));
+app.use(require('./routes/authenthication.js'));
+app.use(require('./routes/routine.js'));
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')));git 
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
+
