4c6974b6e5bb7caf19d089b656bbc635a999dddc
bishuu1 <tlz98x@gmail.com>
2019-06-10 15:38:26 -0400

    limpieza 2.0

--- src/index.js
+++ src/index.js
@@ -8,15 +8,7 @@ const app = express();
 
 //Configuracion
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs',exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs',
-    helpers: require('./lib/handlebars')
-}))
-app.set('view engine', '.hbs');
+//app.set('views', path.join(__dirname, 'views'));
 
 //Middlewares
 app.use(morgan('dev'));
--- src/routes/easyfit.js
+++ src/routes/easyfit.js
@@ -3,26 +3,41 @@ const router = express.Router();
 
 const mysqlConnection = require('../database');
 
+
+
 router.post('/signup', (req,res) => {
     const { username, password, Name, Dateob, SexualGender, Email, Weightkg, Heightcm   } = req.body;
     mysqlConnection.query('INSERT INTO UserPassword ?', [username,password]);
-    mysqlConnection.query('INSERT INTO UserData ?', [username, Name, Dateob, SexualGender, Email])
-    mysqlConnection.query('INSERT INTO UserPhysical ?' [username, Weightkg, Heightcm])
+    mysqlConnection.query('INSERT INTO UserData ?', [username, Name, Dateob, SexualGender, Email]);
+    mysqlConnection.query('INSERT INTO UserPhysical ?' [username, Weightkg, Heightcm]);
     
 });
 
 router.put('/profile', (req,res) => {
-    const { username, password, Name, Dateob, SexualGender, Email, Weightkg, Heightcm   } = req.body;
-    mysqlConnection.query('UPDATE UserPassword ?', [password]);
-    mysqlConnection.query('UPDATE UserData ?', [ Name, Dateob, SexualGender, Email])
-    mysqlConnection.query('UPDATE UserPhysical ?' [ Weightkg, Heightcm])
+    const { username, password, Name, Dateob, SexualGender, Email, Weightkg, Heightcm } = req.body;
+    const editPass = {
+        password,
+    };
+    const editData = {
+        Name,
+        Dateob,
+        SexualGender,
+        Email,
+    };
+    const editPhysical = {
+        Weightkg,
+        Heightcm
+    };
+    mysqlConnection.query('UPDATE UserPassword ? WHERE username = ?', [ editPass, username ]);
+    mysqlConnection.query('UPDATE UserData ? WHERE username = ?', [ editData, username ]);
+    mysqlConnection.query('UPDATE UserPhysical ? WHERE username= ?' [ editPhysical, username ]);
 
 });
 
 
 router.get('/profile', (req,res) => {
     const { username } = req.body;
-    mysqlConnection.query('SELECET * FROM Routine where Username_fk = ? ', [username]);
+    mysqlConnection.query('SELECT * FROM Routine where Username_fk = ? ', [username]);
 });
 
 
