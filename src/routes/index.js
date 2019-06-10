fb2276673e4afa525dd1ffb70c1d104d1f7e232f
bishuu1 <tlz98x@gmail.com>
2019-06-10 00:32:36 -0400

    limpieza

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/*router.get('/', (req, res) => {
-    res.send ();
-});*/
+router.get('/', (req, res) => {
+    res.render ();
+});
 
 module.exports = router;
